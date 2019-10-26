9cb4581b3b637a39daef145194ce966421788554
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 23:44:43 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,11 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
+    end
   end
 
 end
