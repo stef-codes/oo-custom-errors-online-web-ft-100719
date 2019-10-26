1aa5f383cd3c85a676c129445e5fcb44999f0bb1
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 23:43:25 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -12,6 +12,11 @@ class Person
 
 end
 
+# Add the following two lines:
+  class PartnerError < StandardError
+  end
+end
+
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
